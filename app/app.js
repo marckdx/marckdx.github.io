863c9092f4dd11048cd56bdbb8db524928fe9f67
Marco Aurélio <marckdx@outlook.com>
2017-01-02 15:37:09 -0200

    app architect follows MVC pattern

--- app/app.js
+++ app/app.js
@@ -1,4 +1,23 @@
-	
+var app = angular.module('mdx-app',['ngRoute']);
+
+app.config(function($routeProvider, $locationProvider)
+{
+	//Removind # from URL
+	$locationProvider.html5Mode(true);
+
+	$routeProvider
+	.when('/', {
+      templateUrl : 'app/views/home.html',
+      controller     : 'HomeCtrl',
+   })
+	.otherwise ({ redirectTo: '/' });
+});
+
+app.controller('HomeCtrl', function($rootScope, $location)
+{
+   $rootScope.activetab = $location.path();
+});
+
 var app = angular.module('mdx-app', ['pascalprecht.translate']);
 
 app.config(function($translateProvider) {
@@ -16,7 +35,17 @@ app.config(function($translateProvider) {
 
     BUTTON_TEXT_EN: 'English',
     BUTTON_TEXT_PT: 'Portuguese',
-    BUTTON_TEXT_ES: 'Spanish'
+    BUTTON_TEXT_ES: 'Spanish',
+
+    MY_HISTORY: 'MY HISTORY',
+    DOWNLOAD_RESUME: 'DOWNLOAD RESUME',
+    PERSONAL_INFO: 'PERSONAL INFO',
+
+    NAME: 'Name',
+    AGE: 'Age',
+    TEL: 'Phone',
+    EMAIL: 'Email'
+
   })
   .translations('pt', {
     SITE_TITLE: 'MARCKDX - Desenvolvedor de Software',
@@ -32,7 +61,17 @@ app.config(function($translateProvider) {
 
     BUTTON_TEXT_EN: 'Inglês',
     BUTTON_TEXT_PT: 'Português',
-    BUTTON_TEXT_ES: 'Espanhol'
+    BUTTON_TEXT_ES: 'Espanhol',
+
+    MY_HISTORY: 'MINHA HISTÓRIA',
+    DOWNLOAD_RESUME: 'BAIXAR CURRÍCULO',
+    PERSONAL_INFO: 'INFORMAÇÃO PESSOAL',
+
+    NAME: 'Nome',
+    AGE: 'Idade',
+    TEL: 'Telefone',
+    EMAIL: 'Email'
+
   }).translations('es', {
     SITE_TITLE: 'MARCKDX - Desarollador de Software',
     SITE_CUR_LANG: 'Es',
@@ -47,9 +86,18 @@ app.config(function($translateProvider) {
 
     BUTTON_TEXT_EN: 'Inglés',
     BUTTON_TEXT_PT: 'Portugués',
-    BUTTON_TEXT_ES: 'Español'
+    BUTTON_TEXT_ES: 'Español',
+
+    MY_HISTORY: 'MI HISTORIA',
+    DOWNLOAD_RESUME: 'DESCARGAR RESUMEN',
+    PERSONAL_INFO: 'INFORMACIÓN PERSONAL',
+
+    NAME: 'Nombre',
+    AGE: 'Idad',
+    TEL: 'Teléfono',
+    EMAIL: 'Correo'
   });
-  $translateProvider.preferredLanguage('pt');
+  $translateProvider.preferredLanguage('en');
 });
 
 app.controller('TranslateController', function($translate, $scope) {
@@ -57,3 +105,10 @@ app.controller('TranslateController', function($translate, $scope) {
     $translate.use(langKey);
   };
 });
+
+$( document ).ready(function() {
+  $('.year').text(new Date().getFullYear());
+});
+	
+
+
