911962ec152546ffb90c6df21eab88e461f75c5f
Marco Aurélio <marckdx@outlook.com>
2018-01-05 01:39:15 -0200

    adicionado service worker

--- app/app.js
+++
@@ -1,1 +0,0 @@
-angular.module("appMarckDX",  ["chart.js", "angularUtils.directives.dirPagination"]);
---
+++ assets/js/worker.js
@@ -0,0 +1,47 @@
+var CACHE_NAME = 'marckdx-cache-v3';
+
+var urlsToCache = [
+  '/',
+  '/index.html',
+  '/favicon-48x48.png',
+  '/assets/css/styles.css',
+  '/assets/js/app.js',
+  '/assets/js/main.js',
+  '/assets/plugins/bootstrap/css/bootstrap.min.css',
+  '/assets/plugins/bootstrap/js/bootstrap.min.js',
+  '/assets/plugins/bootstrap/js/npm.js',
+  '/assets/plugins/bootstrap/fonts/glyphicons-halflings-regular.eot',
+  '/assets/plugins/bootstrap/fonts/glyphicons-halflings-regular.svg',
+  '/assets/plugins/bootstrap/fonts/glyphicons-halflings-regular.ttf',
+  '/assets/plugins/bootstrap/fonts/glyphicons-halflings-regular.woff',
+  '/assets/plugins/bootstrap/fonts/glyphicons-halflings-regular.woff2',
+  '/assets/plugins/bootstrap/fonts/glyphicons-halflings-regular.svg',
+  '/assets/plugins/font-awesome/css/font-awesome.min.css',
+  '/assets/plugins/font-awesome/css/font-awesome.css',
+  '/assets/plugins/font-awesome/fonts/fontawesome-webfont.eot',
+  '/assets/plugins/font-awesome/fonts/fontawesome-webfont.svg',
+  '/assets/plugins/font-awesome/fonts/fontawesome-webfont.ttf',
+  '/assets/plugins/font-awesome/fonts/fontawesome-webfont.woff',
+  '/assets/plugins/font-awesome/fonts/fontawesome-webfont.woff2',
+  '/assets/plugins/jquery-1.11.3.min.js',
+ ];
+
+ self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log(event.request.url);
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
